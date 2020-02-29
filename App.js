4be071661ffd98565c98c77cd65af229631d28a0
thyagoneck <thyagoneck@gmail.com>
2020-02-29 19:16:27 -0300

    feat: conversor decimal para binario

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, TextInput, Button, View } from "react-native";
 
 export default function App() {
+  const [number, setNumber] = useState(0);
+  const [numberBinary, setBinary] = useState(0);
+
+  const convertNumberToBinary = value => {
+    return (value >>> 0).toString(2);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Digite um número: </Text>
+      <TextInput
+        style={styles.textInputStyle}
+        autoCapitalize="none"
+        keyboardType="numeric"
+        onChangeText={input => {
+          const sanitizedNumber = input.replace(/([^0-9])/g, "");
+          setNumber(sanitizedNumber);
+          const binaryNumber = convertNumberToBinary(sanitizedNumber);
+          setBinary(binaryNumber);
+        }}
+        value={number.toString()}
+      />
+      <Text>{numberBinary}</Text>
     </View>
   );
 }
@@ -12,8 +32,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#ffa000",
+    alignItems: "center",
+    justifyContent: "center"
   },
+  textInputStyle: {
+    height: 80,
+    width: 200,
+    borderColor: "black",
+    borderWidth: 1,
+    borderRadius: 5,
+    marginTop: 10,
+    fontSize: 50,
+    textAlign: "center"
+  }
 });
